a5b56e9ecc161f483a217cb76eb59b79ba219fb8
medicineBear <1147877104@qq.com>
2021-05-23 10:30:08 +0800

    test

---
+++ src/main/java/com/yaorui/test/Hello.java
@@ -0,0 +1,11 @@
+package com.yaorui.test;
+
+/**
+ * @author medicineBear
+ * @create 2021-05-23 10:27
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
